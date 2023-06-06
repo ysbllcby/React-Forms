bae090002b7a16f7e96b3f8aa36d7cb8b721444c
Ma Cubay <ma.cubay@edu.sait.ca>
2023-06-05 21:31:16 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeadingText(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
